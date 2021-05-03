c1a8a108edbd698d46a340e2a2ad09a41eff4f28
Patrick Lee <patricklee1138@live.com>
2021-05-03 15:28:20 -0700

    :white_check_mark: RED: should return 'false' for years divisible by 100 being leap years
    Co-authored-by: Brandon Magofna <Magofna68@gmail.com>

--- Calendar.Tests/ModelTests/LeapYearTests.cs
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -12,11 +12,19 @@ namespace Calendar.TestTools
       LeapYear testLeapYear = new LeapYear();
       Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
     }
+    
     [TestMethod]
     public void IsLeapYear_NumberNotDivisibleByFour_False()
     {
       LeapYear testLeapYear = new LeapYear();
       Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
     }
+
+      [TestMethod]
+      public void IsLeapYear_MultiplesOfOneHundred_False()
+      {
+        LeapYear testLeapYear = new LeapYear();
+        Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+      }
   }
 }
